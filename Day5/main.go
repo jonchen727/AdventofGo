dffbc235300a634c2323a9c67417b73429a2d30c
jonchen727 <29684743+jonchen727@users.noreply.github.com>
2023-10-03 09:43:53 -0700

    add day 5 part 1

---
+++ Day5/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"slices"
+	"strings"
+	//"strconv"
+	"flag"
+	//"sort"
+	//"github.com/jonchen727/2022-AdventofCode/helpers"
+)
+
+//go:embed input.txt
+var input string
+var priorities = map[string]int{}
+
+func init() {
+	input = strings.TrimRight(input, "\n")
+	if len(input) == 0 {
+		panic("input is empty")
+	}
+
+}
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 1, "part of the puzzle to run")
+	flag.Parse()
+	fmt.Println("Part", part)
+
+	if part == 1 {
+		answer := part1(input)
+		fmt.Println("Answer: ", answer)
+
+	} else {
+		ans := part2(input)
+		fmt.Println("Answer:", ans)
+		//fmt.Println("Answer:", ans)
+	}
+
+}
+
+func part1(input string) string {
+	blocks, steps := parseInput(input)
+	var ans string
+	for _, step := range steps {
+		move := blocks[step.from][len(blocks[step.from])-step.amt:]
+		slices.Reverse(move)
+		blocks[step.from] = blocks[step.from][:len(blocks[step.from])-step.amt]
+		blocks[step.to] = append(blocks[step.to], move...)
+		fmt.Println("Move:", move)
+	}
+
+	for _, block := range blocks {
+		ans += block[len(block)-1]
+	}
+	fmt.Println(blocks)
+	return ans
+}
+
+func part2(input string) int {
+	return 0
+}
+
+type command struct {
+	amt  int
+	from int
+	to   int
+}
+
+func parseInput(input string) ([][]string, []command) {
+	split := strings.Split(input, "\n\n")
+	blocks := split[0]
+	rawcommands := split[1]
+
+	fmt.Println(blocks)
+	fmt.Println(rawcommands)
+
+	blocks = strings.ReplaceAll(blocks, "    ", "[0]")
+	blocks = strings.ReplaceAll(blocks, " ", "")
+	blocks = strings.ReplaceAll(blocks, "][", "")
+	blocks = strings.ReplaceAll(blocks, "[", "")
+	blocks = strings.ReplaceAll(blocks, "]", "")
+	splitblocks := [][]string{}
+	sortedblocks := [][]string{}
+
+	for _, row := range strings.Split(blocks, "\n") {
+		splitblocks = append(splitblocks, strings.Split(row, ""))
+	}
+
+	fmt.Println(splitblocks)
+
+	tcol, trow := len(splitblocks[0]), len(splitblocks)
+
+	for c := 0; c < tcol; c++ {
+		stack := []string{}
+		for r := trow - 2; r >= 0; r-- {
+			fmt.Println(splitblocks[r][c])
+			char := splitblocks[r][c]
+			if char != "0" {
+				stack = append(stack, char)
+			}
+
+		}
+		sortedblocks = append(sortedblocks, stack)
+	}
+	fmt.Println(sortedblocks)
+
+	commands := []command{}
+
+	for _, row := range strings.Split(rawcommands, "\n") {
+		org := command{}
+		_, err := fmt.Sscanf(row, "move %d from %d to %d", &org.amt, &org.from, &org.to)
+		if err != nil {
+			panic(err)
+		}
+		//shift from and to to be 0 indexed
+		org.from--
+		org.to--
+		commands = append(commands, org)
+	}
+	fmt.Println(commands)
+
+	return sortedblocks, commands
+}
