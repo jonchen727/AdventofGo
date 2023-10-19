82bbaef0778797984161b1e879aa364cfcac502d
jonchen727 <29684743+jonchen727@users.noreply.github.com>
2023-10-19 10:18:04 -0700

    fix part 1

--- Day20/main.go
+++ Day20/main.go
@@ -44,61 +44,70 @@ func main() {
 }
 
 func part1(input string) int {
-	//ans := 0
-	_, nodes := parseInput(input)
-	m := len(nodes) - 1
+	ans := 0
+	nodes, zeronode := parseInput(input)
 
-	var z *Node
-	for i, kd := range nodes {
-		k := &nodes[i]
-		if nodes[i].value == 0 {
-			z = &nodes[i]
-			continue
-		}
-		p := &kd
-		if k.value > 0 {
-			for j := 0; j < helpers.PosMod(k.value, m); j++ {
-				p = p.right
-			}
-			if k == p {
-				continue
-			}
-			k.right.left = k.left
-			k.left.right = k.right
-			p.right.left = k
-			k.right = p.right
-			p.right = k
-			k.left = p
-		} else {
-			for j := 0; j < helpers.PosMod(k.value, m); j++ {
-				p = p.left
-			}
-			if k == p {
-				continue
-			}
-			k.right.left = k.left
-			k.left.right = k.right
-			p.left.right = k
-			k.left = p.left
-			p.left = k
-			k.right = p
+	// find the node with value 0
 
-		}
-	}
-	t := 0
+	// move nodes
 	for i := 0; i < len(nodes); i++ {
-		fmt.Println(nodes[i].right.value)
+		nodes[i].move(len(nodes))
 	}
 
+	// sum values of nodes clockwise from 0
 	for i := 0; i < 3; i++ {
 		for j := 0; j < 1000; j++ {
-			z = z.right
-
+			zeronode = zeronode.right
 		}
-		t += z.value
+		ans += zeronode.value
+	}
+	return ans
+}
+
+func (node *Node) move(totalLen int) {
+	steps := node.value
+
+	// translate steps into absolute steps
+	steps %= totalLen - 1
+
+	//if steps = 0 then do nothing
+	if steps == 0 {
+		return
+	}
+	//translate backwards into forward steps
+	if steps < 0 {
+		steps += (totalLen - 1)
 	}
 
-	return t
+	// remove node from neighbors and update references
+	// grab current nodes neighbors
+	oldleft, oldright := node.left, node.right
+	// left neighbor now points to nodes previous right neighbor
+	oldleft.right = oldright
+	// right neighbor now points to nodes previous left neighbor
+	oldright.left = oldleft
+
+	// save starting node
+	i := node
+	// move right by steps
+	for steps > 0 {
+		i = i.right
+		if i == node {
+			panic("repeat")
+		}
+		steps--
+	}
+
+	// save values of where to insert node
+	nextleft, nextright := i, i.right
+	// update left neighbor
+	nextleft.right = node
+	// update self to point to left neighbor
+	node.left = nextleft
+	// update right neighbor
+	nextright.left = node
+	// update self to point to right neighbor
+	node.right = nextright
 }
 
 func part2(input string) int {
@@ -112,20 +121,22 @@ type Node struct {
 	right *Node
 }
 
-func parseInput(input string) ([]int, []Node) {
-	numlist := []int{}
+func parseInput(input string) ([]Node, *Node) {
+
+	zeroNode := &Node{}
 	nodes := []Node{}
 	for _, num := range strings.Split(input, "\n") {
-		numlist = append(numlist, helpers.ToInt(num))
 		nodes = append(nodes, Node{value: helpers.ToInt(num)})
 	}
-	fmt.Println(nodes)
+	//fmt.Println(nodes)
 	for i, _ := range nodes {
 		nodes[i].right = &nodes[helpers.PosMod((i+1), len(nodes))]
-		nodes[i].left = &nodes[helpers.PosMod(i-1, len(nodes))]
-
+		nodes[i].left = &nodes[helpers.PosMod((i-1), len(nodes))]
+		if nodes[i].value == 0 {
+			zeroNode = &nodes[i]
+		}
 	}
-	fmt.Println(nodes)
+	//fmt.Println(nodes)
 
-	return numlist, nodes
+	return nodes, zeroNode
 }
