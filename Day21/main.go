112ddfde864d57c0d4e60fa702652714eaa5706c
jonchen727 <29684743+jonchen727@users.noreply.github.com>
2023-10-19 22:33:01 -0700

    fix part 2, error handling 2 missing values

--- Day21/main.go
+++ Day21/main.go
@@ -6,8 +6,8 @@ import (
 	//"slices"
 	"strings"
 	//"reflect"
-	"strconv"
 	"flag"
+	//"strconv"
 	//"math"
 	"time"
 	//"sort"
@@ -34,10 +34,10 @@ func main() {
 	if part == 1 {
 		ans := part1(input)
 
-		fmt.Println("Part 1 Answer:", strconv.FormatFloat(ans, 'f', -1, 64))
+		fmt.Println("Part 1 Answer:", ans)
 	} else {
 		ans := part2(input)
-		fmt.Println("Part 2 Answer:", strconv.FormatFloat(ans, 'f', -1, 64))
+		fmt.Println("Part 2 Answer:", ans)
 	}
 	duration := time.Since(start) //sets duration to time difference since start
 	fmt.Println("This Script took:", duration, "to complete!")
@@ -45,16 +45,16 @@ func main() {
 
 type Monkey struct {
 	name    string
-	value   float64
+	value   int
 	monkey1 *Monkey
 	monkey2 *Monkey
 	op      string
 }
 
-func (m *Monkey) getValue(itr int, part int) float64 {
+func (m *Monkey) getValue(itr int, part int) int {
 
 	if m.name == "root" && part == 2 {
-		
+
 		m.monkey1.getValue(itr, part)
 		m.monkey2.getValue(itr, part)
 		return 1
@@ -62,20 +62,21 @@ func (m *Monkey) getValue(itr int, part int) float64 {
 	}
 
 	if m.value > 0 {
-		return m.value}
+		return m.value
+	}
 
-	if itr >= 0 && (m.monkey1.name == "humn" || m.monkey2.name == "humn") && part == 2 {
+	if itr > 0 && (m.monkey1.name == "humn" || m.monkey2.name == "humn" ) && part == 2 {
 		m.value = -1
 		return m.value
 	}
 	switch m.op {
 	case "+":
 		itr++
-		
+
 		m1 := m.monkey1.getValue(itr, part)
 		m2 := m.monkey2.getValue(itr, part)
 		if checkNeg(m1, m2) {
-			m.value = helpers.MinFloat(m1,m2) - 1
+			m.value = helpers.MinInt(m1, m2) - 1
 		} else {
 			m.value = m.monkey1.getValue(itr, part) + m.monkey2.getValue(itr, part)
 		}
@@ -85,7 +86,7 @@ func (m *Monkey) getValue(itr int, part int) float64 {
 		m1 := m.monkey1.getValue(itr, part)
 		m2 := m.monkey2.getValue(itr, part)
 		if checkNeg(m1, m2) {
-			m.value = helpers.MinFloat(m1,m2) - 1
+			m.value = helpers.MinInt(m1, m2) - 1
 		} else {
 			m.value = m.monkey1.getValue(itr, part) * m.monkey2.getValue(itr, part)
 		}
@@ -95,7 +96,7 @@ func (m *Monkey) getValue(itr int, part int) float64 {
 		m1 := m.monkey1.getValue(itr, part)
 		m2 := m.monkey2.getValue(itr, part)
 		if checkNeg(m1, m2) {
-			m.value = helpers.MinFloat(m1,m2) - 1
+			m.value = helpers.MinInt(m1, m2) - 1
 		} else {
 			m.value = m.monkey1.getValue(itr, part) / m.monkey2.getValue(itr, part)
 		}
@@ -105,39 +106,32 @@ func (m *Monkey) getValue(itr int, part int) float64 {
 		m1 := m.monkey1.getValue(itr, part)
 		m2 := m.monkey2.getValue(itr, part)
 		if checkNeg(m1, m2) {
-			m.value = helpers.MinFloat(m1,m2) - 1
+			m.value = helpers.MinInt(m1, m2) - 1
 		} else {
 			m.value = m.monkey1.getValue(itr, part) - m.monkey2.getValue(itr, part)
 		}
 		return m.value
-	case "==":
-		fmt.Println("==")
-		itr++
-		m1 := m.monkey1.getValue(itr, part)
-		m2 := m.monkey2.getValue(itr, part)
-		m.value = helpers.MaxFloat(m1, m2)
-		return m.value
+
 	default:
-		fmt.Println("default")
+		//fmt.Println("default")
 		return m.value
 	}
 }
 
-func checkNeg(n1, n2 float64) bool {
+func checkNeg(n1, n2 int) bool {
 	if n1 < 0 || n2 < 0 {
 		return true
 	}
 	return false
 }
 
-func part1(input string) float64 {
+func part1(input string) int {
 	monkeys := parseInput(input)
 	root, rootExists := monkeys["root"]
 	if !rootExists {
 		fmt.Println("Root node not found.")
 		panic("Root node not found.")
 	}
-
 
 	itr := 0
 	ans := root.getValue(itr, 1)
@@ -146,11 +140,11 @@ func part1(input string) float64 {
 	return ans
 }
 
-func part2(input string) float64 {
+func part2(input string) int {
 
 	monkeys := parseInput(input)
-  monkeys["root"].op = "/"
-	monkeys["root"].value = float64(1.0000000000000000000000)
+	monkeys["root"].op = "/"
+	monkeys["root"].value = 1
 	monkeys["humn"].value = 0
 	monkeys["humn"].op = "?"
 
@@ -160,45 +154,42 @@ func part2(input string) float64 {
 		panic("Root node not found.")
 	}
 	itr := 0
-	root.getValue(itr,2)
+	root.getValue(itr, 2)
+	
 
-
-	fmt.Println(monkeys["root"].monkey2)
-	fmt.Println(monkeys["root"].monkey1)
+	//fmt.Println(monkeys["root"].monkey2)
+	//fmt.Println(monkeys["root"].monkey1)
 
 	maxct := 0
 	nodemap := map[int][]*Monkey{}
 	for _, v := range monkeys {
 		if v.value < 1 {
-			nodemap[helpers.ToInt(v.value)] = append(nodemap[helpers.ToInt(v.value)], v)
-			maxct = helpers.MinInt(maxct, helpers.ToInt(v.value))
+			nodemap[v.value] = append(nodemap[v.value], v)
+			maxct = helpers.MinInt(maxct, v.value)
 		}
 	}
-
-
 	root.fillMissingVals()
-	for i := maxct; i < 0 ; i++ {
+	for _, val := range nodemap[0] {
+		val.getValue(itr, 2)
+	}
+	for i := maxct; i < 0; i++ {
 		nodes := nodemap[i]
 		for j := 0; j < len(nodes); j++ {
 			nodes[j].fillMissingVals()
-			fmt.Println(nodes[j])
+			//fmt.Println(nodes[j])
 		}
-}
-for _, v := range(monkeys){
-	fmt.Println("Monkey:",v, v.monkey1, v.monkey2)
-}
-// for key, val := range nodemap {
-// 	for j := 0; j < len(val); j++{
-// 		fmt.Println(key, val[j])
-// 	}
-// }
-	//fmt.Println(monkeys["fzvp"].value, monkeys["grlb"].value)
-
-for _, value := range monkeys {
-	if value.value < 0 {
-		fmt.Println("you done fucked up")
 	}
-}
+	// for _, v := range nodemap {
+	// 	for j := 0; j < len(v); j++ {
+	// 		fmt.Println(v[j], v[j].monkey1, v[j].monkey2)
+	// 	}
+	// }
+
+	for _, value := range monkeys {
+		if value.value < 0 {
+			fmt.Println("you done fucked up")
+		}
+	}
 	return monkeys["humn"].value
 }
 func (m *Monkey) fillMissingVals() {
@@ -207,9 +198,11 @@ func (m *Monkey) fillMissingVals() {
 	var missing1 bool
 	if m.monkey1.value <= 0 {
 		missing1 = true
-	} else {
+	} else { 
 		missing1 = false
-	}
+		}
+		
+	
 
 	switch m.op {
 	case "+":
@@ -218,10 +211,10 @@ func (m *Monkey) fillMissingVals() {
 		// n2 = num - n1
 
 		if missing1 {
-			fmt.Println("x=", m.value, "-", m.monkey2.value)
+			// fmt.Println("x=", m.value, "-", m.monkey2.value)
 			m.monkey1.value = m.value - m.monkey2.value
 		} else {
-			fmt.Println("x=", m.value, "-", m.monkey1.value)
+			// fmt.Println("x=", m.value, "-", m.monkey1.value)
 			m.monkey2.value = m.value - m.monkey1.value
 		}
 	case "*":
@@ -229,23 +222,23 @@ func (m *Monkey) fillMissingVals() {
 		// n1 = num / n2
 		// n2 = num / n1
 		if missing1 {
-			fmt.Println("x=", m.value, "/", m.monkey2.value)
-			m.monkey1.value = float64(m.value) / float64(m.monkey2.value)
+			// fmt.Println("x=", m.value, "/", m.monkey2.value)
+			m.monkey1.value = m.value / m.monkey2.value
 		} else {
-			fmt.Println("x=", m.value, "/", m.monkey1.value)
-			m.monkey2.value = float64(m.value) / float64(m.monkey1.value)
+			// fmt.Println("x=", m.value, "/", m.monkey1.value)
+			m.monkey2.value = m.value / m.monkey1.value
 		}
 	case "/":
 		// num = n1 / n2
 		// n1 = num * n2
 		// n2 = n1/num
 		if missing1 {
-			fmt.Println("x=", m.value, "*", m.monkey2.value)
+			// fmt.Println("x=", m.value, "*", m.monkey2.value)
 			m.monkey1.value = m.value * m.monkey2.value
 		} else {
-			
-			fmt.Println("x=", m.value, "*", m.monkey1.value)
-			m.monkey2.value =  float64(m.monkey1.value) / float64(m.value)
+
+			// fmt.Println("x=", m.value, "*", m.monkey1.value)
+			m.monkey2.value = m.monkey1.value / m.value
 		}
 
 	case "-":
@@ -253,10 +246,10 @@ func (m *Monkey) fillMissingVals() {
 		// n1 = num + n2
 		// n2 = n1 - num
 		if missing1 {
-			fmt.Println("x=", m.value, "+", m.monkey2.value)
+			// fmt.Println("x=", m.value, "+", m.monkey2.value)
 			m.monkey1.value = m.value + m.monkey2.value
 		} else {
-			fmt.Println("x=", m.value, "+", m.monkey1.value)
+			// fmt.Println("x=", m.value, "+", m.monkey1.value)
 			m.monkey2.value = m.monkey1.value - m.value
 		}
 	}
@@ -270,7 +263,7 @@ func parseInput(input string) map[string]*Monkey {
 		name := split[0]
 		data := strings.Split(split[1], " ")
 		if len(data) == 1 {
-			val, _ := strconv.ParseFloat(data[0], 64)
+			val := helpers.ToInt(data[0])
 			m := Monkey{name: name, value: val}
 			monkeys[name] = &m
 		} else {
