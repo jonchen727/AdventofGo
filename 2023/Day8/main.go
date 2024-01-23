cd5ad16832d125f09cea3a15386a229fffb54a18
jonchen727 <29684743+jonchen727@users.noreply.github.com>
2024-01-23 15:10:01 -0800

    add day 8 part 2

--- 2023/Day8/main.go
+++ 2023/Day8/main.go
@@ -44,14 +44,14 @@ func main() {
 }
 
 func part1(input string) int {
-  ans := 0
+	ans := 0
 	inst, node := parseInput(input)
 	start := "AAA"
 	for true {
 		if start == "ZZZ" {
 			break
 		}
-		
+
 		switch inst[ans%len(inst)] {
 		case "R":
 			start = node[start][1]
@@ -64,20 +64,83 @@ func part1(input string) int {
 }
 
 func part2(input string) int {
-  ans := 0
-	return ans
+
+	inst, node := parseInput(input)
+	start := []string{}
+	for key, _ := range node {
+		if strings.Split(key, "")[2] == "A" {
+			start = append(start, key)
+		}
+	}
+
+	loopLengths := [][]int{}
+	for _, val := range start {
+		loopLengths = append(loopLengths, findLoop(node, val, inst))
+	}
+
+	fmt.Println(loopLengths)
+	factors := []int{}
+	for _, num := range loopLengths {
+		factors = append(factors, num[0])
+	}
+
+	lcm := factors[0]
+	factors = factors[1:]
+
+	for _, num := range factors {
+		lcm = lcm * num / gcd(lcm, num)
+	}
+
+	return lcm
+}
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
 }
 
-func parseInput(input string)([]string, map[string][]string){
+func findLoop(nodes map[string][]string, start string, inst []string) []int {
+	current := start
+	steps := 0
+	loop := []int{}
+	var firstZ string
+
+	for true {
+		for steps == 0 || !strings.HasSuffix(current, "Z") {
+			current = getNextNode(nodes, current, inst[steps%len(inst)])
+			steps++
+		}
+		loop = append(loop, steps)
+		if firstZ == "" {
+			firstZ = current
+			steps = 0
+		} else if current == firstZ {
+			break
+		}
+	}
+	return loop
+}
+
+func getNextNode(nodes map[string][]string, current string, inst string) string {
+	var next string
+	if inst == "L" {
+		next = nodes[current][0]
+	} else { // Assuming "R"
+		next = nodes[current][1]
+	}
+	return next
+}
+
+func parseInput(input string) ([]string, map[string][]string) {
 	split := strings.Split(input, "\n\n")
 	inst := []string{}
 
-	for _, val := range strings.Split(split[0],""){
+	for _, val := range strings.Split(split[0], "") {
 		inst = append(inst, string(val))
 	}
 
 	node := make(map[string][]string)
-	fmt.Println(split[1])
 	for _, val := range strings.Split(split[1], "\n") {
 		split2 := strings.Split(val, " = ")
 		parts := strings.Split(strings.Trim(split2[1], "()"), ", ")
