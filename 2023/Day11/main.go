6079e13c25070db10fa35ec131519609e2713bce
jonchen727 <29684743+jonchen727@users.noreply.github.com>
2024-01-30 08:38:09 -0800

    add day 11 part 1

---
+++ 2023/Day11/main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	//"slices"
+	"strings"
+	//"reflect"
+	//"strconv"
+	"flag"
+	//"math"
+	"time"
+	//"sort"
+	"github.com/jonchen727/AdventofGo/helpers"
+)
+
+//go:embed input.txt
+var input string
+
+func init() {
+	input = strings.TrimRight(input, "\n")
+	if len(input) == 0 {
+		panic("input is empty")
+	}
+
+}
+
+func main() {
+	start := time.Now()
+	var part int
+	flag.IntVar(&part, "part", 1, "part of the puzzle to run")
+	flag.Parse()
+
+	if part == 1 {
+		ans := part1(input)
+
+		fmt.Println("Part 1 Answer:", ans)
+	} else {
+		ans := part2(input)
+		fmt.Println("Part 2 Answer:", ans)
+	}
+	duration := time.Since(start) //sets duration to time difference since start
+	fmt.Println("This Script took:", duration, "to complete!")
+}
+
+func part1(input string) int {
+	ans := 0
+	galaxies, rmax, cmax := parseInput(input)
+	expandSpace(galaxies, rmax, cmax)
+
+	for i := 0; i < len(galaxies); i++ {
+		for j := i + 1; j < len(galaxies); j++ {
+			ans += ManhattanDistance(galaxies[i], galaxies[j])
+		}
+	}
+	return ans
+}
+
+func part2(input string) int {
+	ans := 0
+	return ans
+}
+
+type Galaxy struct {
+	r      int
+	c      int
+	symbol string
+}
+
+func ManhattanDistance(g1, g2 Galaxy) int {
+	return helpers.Abs(g1.r-g2.r) + helpers.Abs(g1.c-g2.c)
+}
+
+func expandSpace(galaxies []Galaxy, rmax int, cmax int) {
+	rspace := map[int][]*Galaxy{}
+	//rkey := []int{}
+	cspace := map[int][]*Galaxy{}
+	//ckey := []int{}
+	for g, galaxy := range galaxies {
+		// make a map of all row values that are occupied
+		for i := 0; i < rmax; i++ {
+			if i == galaxy.r {
+				if _, okr := rspace[galaxy.r]; !okr {
+					rspace[i] = []*Galaxy{&galaxies[g]}
+				} else {
+					rspace[i] = append(rspace[i], &galaxies[g])
+				}
+			}
+		}
+		// make a map of all column values that are occupied
+		for i := 0; i < cmax; i++ {
+			if i == galaxy.c {
+				if _, okc := cspace[galaxy.c]; !okc {
+					cspace[i] = []*Galaxy{&galaxies[g]}
+				} else {
+					cspace[i] = append(cspace[i], &galaxies[g])
+				}
+			}
+		}
+	}
+	// expand rows
+	for i := 0; i < rmax; i++ {
+		if _, ok := rspace[i]; !ok {
+			for j := i + 1; j < rmax; j++ {
+				for k, _ := range rspace[j] {
+					rspace[j][k].r += 1
+				}
+			}
+		}
+	}
+
+	for i := 0; i < cmax; i++ {
+		if _, ok := cspace[i]; !ok {
+			for j := i + 1; j < cmax; j++ {
+				for k, _ := range cspace[j] {
+					cspace[j][k].c += 1
+				}
+			}
+		}
+	}
+}
+
+func parseInput(input string) ([]Galaxy, int, int) {
+	galaxies := []Galaxy{}
+	rmax := len(strings.Split(input, "\n"))
+	cmax := len(strings.Split(strings.Split(input, "\n")[0], ""))
+	for r, line := range strings.Split(input, "\n") {
+		for c, char := range strings.Split(line, "") {
+			if string(char) != "." {
+				galaxy := Galaxy{
+					r:      r,
+					c:      c,
+					symbol: string(char),
+				}
+				galaxies = append(galaxies, galaxy)
+			}
+		}
+	}
+	return galaxies, rmax, cmax
+}
